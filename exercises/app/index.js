7e82f7fccda7b6386d5605bc6a24f5d2bbdd463d
Abhay Chauhan <chauhan27.abhay@gmail.com>
2021-09-22 14:31:30 +0530

    updates

--- exercises/app/index.js
+++ exercises/app/index.js
@@ -1,5 +1,10 @@
+// express - a framework for building servers
+// morgan = a middleware for logging incoming requests
+
+// cast error - you gave it a different value type then what its expecting
+
 const express = require('express')
-const morgan = require('morgan') // give info about request made
+const morgan = require('morgan')
 const connect = require('../connect')
 
 const app = express()
@@ -11,19 +16,19 @@ app.use(express.json())
 
 app.get('/todo/:id', async (req, res) => {
   const todoId = req.params.id
-  const todo = await Todo.findById(id).exec()
+  const todo = await Todo.findById(todoId).exec()
   res.status(200).json(todo)
 })
 
 app.get('/todos', async (req, res) => {
-  const todos = await Todo.find({}).lean().exec()
+  const todos = await Todo.find({}).lean().exec() // lean() -> to object
   res.status(200).json(todos)
 })
 
 app.post('/todo', async (req, res) => {
   const todoToCreate = req.body.todo
   const todo = await Todo.create(todoToCreate) // cannot use lean() or exec() here becoz not a query
-  res.status(201).json(todo.toJSON())
+  res.status(201).json(todo.toJSON()) // .toJSON() -> it gives you back an object
 })
 
 connect('mongodb://localhost:27017/intro-to-mongodb')
